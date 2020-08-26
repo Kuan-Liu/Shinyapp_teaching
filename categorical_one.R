c435d687900436b0d9e123f3b2ea363d7be7b39a
Kuan Liu <45974798+Kuan-Liu@users.noreply.github.com>
2020-08-25 23:08:01 -0400

    update code

--- categorical_one.R
+++ categorical_one.R
@@ -12,7 +12,7 @@ ui <- fluidPage(
   # Sidebar with reactive inputs
   sidebarLayout(
     sidebarPanel(
-      textInput("name",label = "Variable Name"),
+      textInput("name",label = "Variable Name",value="clear all data first"),
       textInput("category",label ="Category"),
       numericInput("count",label ="Frequency", value=1, min=1,max=NA),
       actionButton("save","Add"),
@@ -30,7 +30,7 @@ ui <- fluidPage(
     
     
   )
-
+  
   
 )
 
@@ -41,18 +41,20 @@ server <- function(input, output,session) {
   #create a data frame called responses
   saveData <- function(data) {
     data <- as.data.frame(t(data))
+    
     if (exists("responses")) {
       responses <<- rbind(responses, data)
-     
+      
     } else {
       responses <<- data
-
+      
     }
+    
   }
   
   loadData <- function() {
     if (exists("responses")) {
-     
+      
       responses
     }
   }
@@ -72,7 +74,10 @@ server <- function(input, output,session) {
   #formData is a reactive function
   formData <- reactive({
     data <- sapply(fields[2:3], function(x) input[[x]])
-   
+    # data$count<-as.numeric(data$count)  
+    # rbind(input[[fields[2]]], as.numeric(input[[fields[3]]]))
+    
+    # sapply(fields[2:3], function(x) input[[x]])
   })
   
   # When the Save button is clicked, save the form data
@@ -91,51 +96,49 @@ server <- function(input, output,session) {
   output$responses <- DT::renderDataTable({
     input$save
     loadData()
+    
   })     
   
-
+  
   
   # generate bar chart
   output$barplot <- renderPlot({
-
-    plotdata<-data.frame({
-      input$save
-      loadData()
-    })
     
-    plotdata$count<-c(levels(plotdata$count))
-  
+    plotdata<-rbind( {input$save
+      loadData()})
+    # print(plotdata)
+    
+    
     
     if (is.null(plotdata$category)==F & is.null(plotdata$count)==F){
-    ggplot(data=plotdata, aes(x=category, y=as.numeric(count))) +
-      geom_bar(stat="identity", fill="steelblue")+
-      theme_minimal()+labs(title=paste("Bar Plot of", input[[fields[1]]], sep=" "),
-                           x ="Category", y = "Frequency counts")}
+      ggplot(data=plotdata, aes(x=category, y=as.numeric(count))) +
+        geom_bar(stat="identity", fill="steelblue")+
+        theme_minimal()+labs(title=paste("Bar Plot of", input[[fields[1]]], sep=" "),
+                             x ="Category", y = "Frequency counts")
+      
+    }
   })
   
   output$piechart <- renderPlot({
-
-    plotdata2<-data.frame({
-      input$save
-      loadData()
-    })
-
-    plotdata2$count<-c(levels(plotdata2$count))
-
+    
+    plotdata2<-rbind( {input$save
+      loadData()})
+    
+    
     if (is.null(plotdata2$category)==F & is.null(plotdata2$count)==F){
-
+      
       ggplot(data=plotdata2, aes(x=0, y=as.numeric(count), fill=category))+
         geom_bar(width = 1, stat = "identity") + coord_polar("y", start=0)+
         scale_fill_brewer(palette="Blues")+
         theme(axis.text.x=element_blank(), axis.text.y=element_blank(),panel.background = element_blank())+
         labs(title=paste("Pie Chart of", input[[fields[1]]], sep=" "), x ="Category", y = "Frequency counts")
-
+      
     }
     
     
   })
   
-
+  
   
 }
 
